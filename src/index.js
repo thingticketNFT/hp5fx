e276b76321ce9c7d5217340640ae801ea7393141
thingticketNFT <90926620+thingticketNFT@users.noreply.github.com>
2021-12-21 02:09:00 -0700

    Update index.js
    added font support. loaded image into hydra buffer. added a p5 shape.
    
    based on p5 sketch 'TURDULENCE 2' by remarkability (https://editor.p5js.org/remarkability/sketches/xo_ZdiFNL)

--- src/index.js
+++ src/index.js
@@ -66,7 +66,14 @@ window.$fxhashFeatures = {
 
  }
 
+document.addEventListener("keyup", (event) => {
+    if (event.key === "s" || event.key === "S") {
+        event.preventDefault();
+        screencap();
+    }
+})
 
+speed=0.5
 // p5 text canvas setup
 
 let hydra, hydraCanvas;
@@ -82,48 +89,139 @@ hydraCanvas.getContext("webgl", { preserveDrawingBuffer: false });
 
 
 var elt;
-const s = ( p1 ) => {
 
-let x = 800;
+
+const sa = ( p ) => {
+
+let x = 400;
 let y = 800;
   
-	p1.setup = () => {
-	let canvas = p1.createCanvas(hydraCanvas.width, hydraCanvas.height);
-		p1.pixelDensity(2);
+	p.setup = () => {
+	let canvas = p.createCanvas(hydraCanvas.width, hydraCanvas.height);
+//		p.pixelDensity(2);
 		elt = canvas.elt;
 		s0.init({src: elt})
 		canvas.hide();
-		textArray = ["testing","1","2","3",""]
-		p1.textSize(letterSize)
-		p1.fill(200,200,200);
-		p1.draw = () => {
-			p1.clear()
+		p.textFont('PressStart2P-Regular');
+
+		textArray = ["testing","1","2","3"]
+		textArray2 = ["i","hate","every","bureaucrat"]
+		p.textSize(letterSize)
+
+		p.fill(200,100,200);
+
+		p.draw = () => {
+			p.clear()
 			index = Math.floor(time*0.666%textArray.length)
-			p1.text(textArray[index], ((hydraCanvas.width/2)-(0.93*letterSize)), (hydraCanvas.height/2)+(0.27*letterSize))
+			index2 = Math.floor(time*0.5%textArray2.length)
+			p.fill(255,255,255)
+//			p.ellipse(rnd_int(0,hydraCanvas.width), rnd_int(0,hydraCanvas.height),rnd_btw(10,200));
+
+//			p.text('blah', rnd_int(0,hydraCanvas.width), rnd_int(0,hydraCanvas.height))
+			
+			
+			
+			
+			
+//		p.textFont('PressStart2P-Regular');
+			p.text( textArray[index], ((.4*hydraCanvas.width)+70*(Math.sin(time/4))), (.7*hydraCanvas.height))
+			p.text( textArray2[index2], hydraCanvas.width/4, hydraCanvas.height/3)
 		}
 	};
 };
 
-let myp5 = new p5(s);
 
+
+
+//global on-demand mode
+new p5(sa);
+
+
+
+const sb = ( p ) => {
+
+let x = 400;
+let y = 800;
+  
+	p.setup = () => {
+	let canvas = p.createCanvas(hydraCanvas.width, hydraCanvas.height);
+		p.pixelDensity(2);
+		elt = canvas.elt;
+		s1.init({src: elt})
+		canvas.hide();
+		p.textFont('OpenSans-Bold');
+
+		p.textSize(rnd_btw(30,200))
+
+
+		p.draw = () => {
+//			p.clear()
+			index3 = Math.floor(time*0.666%textArray.length)
+			index4 = Math.floor(time*0.5%rnd_btw(5,10))
+			p.fill(rnd_int(0,255), Math.sin(p.frameCount*.1)*255, rnd_btw(200,255))
+			p.ellipse(rnd_int(0,hydraCanvas.width), rnd_int(0,hydraCanvas.height),rnd_btw(10,200));
+
+			p.text('blah', rnd_int(0,hydraCanvas.width), rnd_int(0,hydraCanvas.height))
+			
+			
+			
+			
+			
+//		p.textFont('PressStart2P-Regular');
+//			p.text( textArray[index], (.05*hydraCanvas.width/(Math.sin(time/4))), (hydraCanvas.height/2)+(0.27*letterSize))
+//			p.text( textArray2[index2], ((hydraCanvas.width/4)-(0.93*letterSize)), (hydraCanvas.height/3)+(0.27*letterSize))
+		}
+	};
+};
+
+
+
+
+//global on-demand mode
+new p5(sb);
+
+
+
+
+s2.initImage("turd.png")
+src(s2).invert(0).scrollY(1,-.2).out(o2)
 
 // hydra variables
 
 relativeScale = window.innerHeight/window.innerWidth
-letterSize = .2*hydraCanvas.width
+letterSize = .05*hydraCanvas.width
+
 
 
 // thingticketNFT hydra code
 
 src(o0)
-osc(5*x1,x1,1)
+voronoi(8,1)
+.mult(osc(10,0.1,()=>Math.sin(time)*3).saturate(3).kaleid(3))
+.modulate(o0,.5)
+//.blend(src(o2).scrollY(0.01).invert(1),0.5)
+//.scrollY(-0.01)
+.scale(0.99)
+.luma(.3)
+.scrollY(-0.01)
+.modulate(voronoi(4,1),0.008)
+.shift( 0.3, 0, 0, 4.4 )
+//.saturate( 5.01 )
+
+
+.diff(osc(2,0.5,1))
 
 .out(o0)
 
 
+
+
+
 src(s0)
 
-	.blend(src(o0))
-
+	.add(src(o0))
+	.add(src(o0))
+		.diff(src(o2))
+		.blend(src(s1),.4)
 .out(o1)
 render(o1)
